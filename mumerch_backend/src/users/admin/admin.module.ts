fddc9e76103edc4f41962987c3ecd2a6ee0a9dae
mcyandex <miguelcyandex130@gmail.com>
2023-06-19 23:19:29 +0600

    Admin CRUD part1 added

--- mumerch_backend/src/users/admin/admin.module.ts
+++ mumerch_backend/src/users/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UnitService } from 'src/models/unit/unit.service';
 import { SizeService } from 'src/models/size/size.service';
 import { AdminController } from './admin.controller';
+import { UserService } from 'src/models/user/user.service';
 
 @Module({
   imports: [],
   controllers: [AdminController],
-  providers: [UnitService, SizeService],
+  providers: [UnitService, SizeService, UserService],
 })
 export class AdminModule {}

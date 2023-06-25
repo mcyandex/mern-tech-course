3beb0bf3ae4214065545d9d3eed2502a921f0c2a
mcyandex <miguelcyandex130@gmail.com>
2023-06-25 19:30:42 +0600

    Update error fixed

--- mumerch_backend/src/models/unit/unit.service.ts
+++ mumerch_backend/src/models/unit/unit.service.ts
@@ -3,14 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UnitEntity } from './unit.entity';
 import { DeleteResult, ILike, Repository } from 'typeorm';
 import { UnitDTO, UnitRegistrationDTO } from './unit.dto';
-import { UserEntity } from '../user/user.entity';
 
 @Injectable()
 export class UnitService {
   constructor(
     @InjectRepository(UnitEntity) private unitRepo: Repository<UnitEntity>,
-    @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
-  ) { }
+  ) {}
 
   async getUnitWithUserInfo(): Promise<UnitEntity[]> {
     return await this.unitRepo.find({ relations: ['user'] });
@@ -28,17 +26,8 @@ export class UnitService {
     })
   }
   async updateUnit(qry:any, data: UnitDTO): Promise<UnitDTO> {
-    const unit = await this.unitRepo.findOneBy({id:qry.id})
-    if(unit==null){
-      throw new NotFoundException(`Unit with ID ${qry.id} not found`);
-    }
-    else{
-      const user = await this.userRepo.findOneBy({id:qry.id})
-      unit.user=user
-      unit.name=data.name
-      await this.unitRepo.save(unit)
-      return this.unitRepo.findOneBy({id:unit.id})
-    }
+    await this.unitRepo.update(qry.id, data)
+    return await this.unitRepo.findOneBy({id:qry.id})
   }
   async deleteUnit(id: string): Promise<DeleteResult> {
     return await this.unitRepo.delete(id);

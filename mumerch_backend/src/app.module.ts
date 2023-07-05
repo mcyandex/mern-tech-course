a11236295add19a720c35347fd0fd719265932a1
mcyandex <miguelcyandex130@gmail.com>
2023-07-06 01:04:03 +0600

    nothing

--- mumerch_backend/src/app.module.ts
+++ mumerch_backend/src/app.module.ts
@@ -26,8 +26,8 @@ import { MailerModule } from '@nestjs-modules/mailer';
         ignoreTLS: true,
         secure: true,
         auth: {
-          user: 'team.robust.dev@gmail.com',
-          pass: 'tjffyyrrradnpupj'
+          user: 'your email',
+          pass: 'generated-password'
         },
       }
     })

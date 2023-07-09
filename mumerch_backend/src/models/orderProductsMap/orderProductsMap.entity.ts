84d6e71c33c275a17f1dde6abf9739a59c09d4e2
mcyandex <miguelcyandex130@gmail.com>
2023-07-09 16:25:07 +0600

    OrderProductsMap entity updated

---
+++ mumerch_backend/src/models/orderProductsMap/orderProductsMap.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { OrderEntity } from "../order/order.entity";
+import { ProductEntity } from "../product/product.entity";
+
+
+@Entity('ProductOrderMap')
+export class OrderProductsMapEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  orderQuantity:number
+  @Column() 
+  orderPrice:number
+  @ManyToOne(()=>OrderEntity, order=>order.orderProducts, {cascade:true})
+  order:OrderEntity
+  @ManyToOne(()=>ProductEntity, product=>product.orderProductsMap, {cascade:true})
+  product:ProductEntity
+}

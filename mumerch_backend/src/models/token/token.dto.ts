cbfc64eeb78becbc272176ee6c9850bd2fa56dd4
nayeemahoque <113884690+nayeemahoque@users.noreply.github.com>
2023-07-17 01:21:21 +0600

    Validation done

--- mumerch_backend/src/models/token/token.dto.ts
+++ mumerch_backend/src/models/token/token.dto.ts
@@ -1,9 +1,14 @@
+import { IsDateString, IsNotEmpty, IsString } from "class-validator";
 import { LoginEntity } from "../login/login.entity";
 
 export class TokenDTO{
   id: string;
+  @IsDateString({},{message:"Date must be a string"})
   startTime: Date
+  @IsDateString({},{message:"Date must be a string"})
   endTime: Date
+  @IsNotEmpty({ message: "Must have a value" })
+  @IsString({message:"Token must be a string"})
   token:string
   login: LoginEntity
 }

7fd1259ad9574913b6d84eb8db90115c605f3d92
mcyandex <miguelcyandex130@gmail.com>
2023-07-17 01:18:17 +0600

    validation added on band... gig... both manager

--- mumerch_backend/src/models/band/band.dto.ts
+++ mumerch_backend/src/models/band/band.dto.ts
@@ -1,40 +1,26 @@
 import { IsDate, IsDateString, IsEmail, IsNotEmpty, IsString, Matches } from "class-validator";
 import { LoginDTO } from "../login/login.dto";
 import { BandManagerDTO } from "../bandManager/bandManager.dto";
+import { LoginEntity } from "../login/login.entity";
+import { BandManagerEntity } from "../bandManager/bandManager.entity";
 
-export class BandDTO{
-    id:string
-    @IsNotEmpty({message:"Name must have a value"})
-    @IsString({message:"Name must have a string"})
-    @Matches(/^[A-Z][a-zA-Z ]+$/, {message:"Enter a proper name"})
-    name:string
-
-    // @IsDate()
-    // @IsNotEmpty({message:"Start date is required"})
-    // onBoardDate: Date
-
-    // @IsNotEmpty({message:"Updater Name must have a value"})
-    // updatedBy:string
-    
-    image:string
-    login:LoginDTO
-    bandManager:BandManagerDTO
+export class BandDTO {
+  id: string;
+  @IsNotEmpty({ message: "Name must have a value" })
+  @IsString({ message: "Name must have a string" })
+  @Matches(/^[A-Z][a-zA-Z ]+$/, { message: "Enter a proper name" })
+  name: string;
+  image: string;
+  login: LoginEntity
+  @IsNotEmpty()
+  bandManager: BandManagerEntity
 }
 
-export class BandResgistrationDTO{
-    @IsNotEmpty({message:"Name must have a value"})
-    @IsString({message:"Name must have a string"})
-    @Matches(/^[A-Z][a-zA-Z ]+$/, {message:"Enter a proper name"})
-    name:string
-
-    // @IsDate()
-    // @IsNotEmpty({message:"Start date is required"})
-    // onBoardDate: Date
-
-    // @IsNotEmpty({message:"Updater Name must have a value"})
-    // updatedBy:string
-    
-    image:string
-    login:LoginDTO
-    //bandManager:BandManagerDTO
+export class BandResgistrationDTO {
+  @IsNotEmpty({ message: "Name must have a value" })
+  @IsString({ message: "Name must have a string" })
+  @Matches(/^[A-Z][a-zA-Z ]+$/, { message: "Enter a proper name" })
+  name: string
+  image: string
+  login: LoginDTO
 }
--- mumerch_backend/src/models/bandManager/bandManager.dto.ts
+++ mumerch_backend/src/models/bandManager/bandManager.dto.ts
@@ -1,20 +1,21 @@
-import {IsNotEmpty} from "class-validator";
+import { IsNotEmpty } from "class-validator";
 import { LoginDTO } from "../login/login.dto";
+import { LoginEntity } from "../login/login.entity";
+import { BandEntity } from "../band/band.entity";
 
-export class BandManagerDTO{
-    id: string
-    // bandId:string
-    // userId:string
-    login:LoginDTO
-
+export class BandManagerDTO {
+  id: string
+  login: LoginEntity
+  @IsNotEmpty({ message: "Band Id required" })
+  band: BandEntity
+  @IsNotEmpty({ message: "Band Manager Id required" })
+  bandManager: BandEntity
 }
-
-
-export class BandManagerRegistrationDTO{
-    @IsNotEmpty()
-    id:string
-    login:LoginDTO
-    
-    // @IsNotEmpty()
-    // userId:string
+export class BandManagerRegistrationDTO {
+  id: string
+  login: LoginEntity
+  @IsNotEmpty({ message: "Band Id required" })
+  band: BandEntity
+  @IsNotEmpty({ message: "Band Manager Id required" })
+  bandManager: BandEntity
 }

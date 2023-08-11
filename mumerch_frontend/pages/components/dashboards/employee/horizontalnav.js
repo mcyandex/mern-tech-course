06148ce6de1e16a8842842cc58cbc4ec70759b57
AfreenSadia <84623357+AfreenSadia@users.noreply.github.com>
2023-08-11 23:30:23 +0600

    file updated

---
+++ mumerch_frontend/pages/components/dashboards/employee/horizontalnav.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function HorizontalNav(props) {
+    const uname = props.uname !=null?"":props.uname
+  return (
+    <>
+      <nav>
+        <div>
+          <Link href='/dashboards/employee/employeedashboard'>
+            <Image src='/logo/mumerch_logo.png' width={125} height={70} alt="Company Logo" />
+          </Link>
+        </div>
+        <ul>
+          <li><Link href='/dashboards/employee/userprofile/userprofile'>Welcome, Sadia</Link></li>
+          <li>
+            <Link href='/dashboards/employee/notifications/notificationlist'>
+              <Image src='/icons/notifications.png' width={30} height={30} alt="Notifications" />
+            </Link>
+          </li>
+          <li>
+            <Link href='/dashboards/employee/changepassword/changepassword'>
+              <Image src='/icons/settings.png' width={30} height={30} alt="Settings" />
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+              <Image src='/icons/logout.png' width={30} height={30} alt="logout" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+  }
